727ed672cc2c473639017581c6c8261c6f8b70df
GUIME00 <guibrasilpereira@outlook.com>
2025-05-15 14:45:32 -0300

    Update ativ55.py

--- Atividades/ativ55.py
+++ Atividades/ativ55.py
@@ -1,9 +1,9 @@
 # #Atividade 55
-palavra = input("Digite a primeira palavra: ")
-palavra_ = input("Digite a segunda palavra: ")
-if len(palavra) > len(palavra_):
-    print(palavra)
-elif len(palavra) < len(palavra_):
-    print(palavra_)
-else:
-    print("As palavras tem o mesmo tamanho.")
+# palavra = input("Digite a primeira palavra: ")
+# palavra_ = input("Digite a segunda palavra: ")
+# if len(palavra) > len(palavra_):
+#     print(palavra)
+# elif len(palavra) < len(palavra_):
+#     print(palavra_)
+# else:
+#     print("As palavras tem o mesmo tamanho.")
