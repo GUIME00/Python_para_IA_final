ace376a4f47b516cffa06ee9c98989c4669668bd
GUIME00 <guibrasilpereira@outlook.com>
2025-05-19 17:01:43 -0300

    Fun

---
+++ Atividades/Função/função4.py
@@ -0,0 +1,16 @@
+# #Atividade 4 de Função
+# numero = int(input("Digite o tamanho desejado: "))
+# def mesaXadrez(tamanho):
+#     linha = 0
+#     while linha < tamanho:
+#         coluna = 0
+#         linha_txt = ""
+#         while coluna < tamanho:
+#             if (linha + coluna) % 2 == 0:
+#                 linha_txt += "1"
+#             else:
+#                 linha_txt += "0"
+#             coluna += 1
+#         print(linha_txt)
+#         linha += 1
+# mesaXadrez(numero)
---
+++ teste.py
@@ -0,0 +1,26 @@
+# #Teste 1
+# numero = int(input("Digite o número: "))
+# contador = 0
+# elemento = ""
+# coluna = 0
+# while contador < numero:
+#     while coluna < numero:
+#         elemento += "letra"
+#         coluna += 1
+#     print(elemento)
+#     contador += 1
+
+# #Teste 2
+# numero = int(input("Digite o número: "))
+# contador = 0
+# while contador < numero:
+#     elemento = ""
+#     coluna = 0
+#     while coluna < numero:
+#         if (coluna + contador) % 2 == 0:
+#             elemento += "0"
+#         else:
+#             elemento += "1"
+#         coluna += 1
+#     print(elemento)
+#     contador += 1
