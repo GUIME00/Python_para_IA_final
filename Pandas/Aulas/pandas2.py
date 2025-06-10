be4c38571915362cd286ccce8702f311d3e3486d
GUIME00 <guibrasilpereira@outlook.com>
2025-06-10 16:58:44 -0300

    Pds

--- Pandas/Aulas/pandas2.py
+++ Pandas/Aulas/pandas2.py
@@ -1,8 +1,8 @@
-# import pandas as pd
+import pandas as pd
 
-# url_filmes = "IMDB-Movie-Data.csv" # "Construir" o caminho para o arquivo
-# df_filmes = pd.read_csv(url_filmes)
-# print("Dados carregados com succeso!")
+url_filmes = "IMDB-Movie-Data.csv" # "Construir" o caminho para o arquivo
+df_filmes = pd.read_csv(url_filmes)
+print("Dados carregados com succeso!")
 
 # # .head() # "Topo"
 
@@ -31,3 +31,49 @@
 
 # print("\nInformações do índice:")
 # print(df_filmes.index)
+
+# # Selecionando a coluna 'Title'
+# titulos_filmes = df_filmes['Series_Title']
+# print("Primeiros 5 títulos.")
+# print(titulos_filmes.head())
+
+# # Selecionando múltiplas colunas
+# filmes_selecionados = df_filmes[['Series_Title','Genre','IMDB_Rating']]
+# print(f"\nDataFrame com título, Gênero e Nota.")
+# print(filmes_selecionados.head())
+
+# # Selecionando a primeira linha
+# # Iloc é usado para selecionar linhas por índice numérico
+# primeira_linha = df_filmes.iloc[200]
+# print(f"\nPrimeira linha selecionada.")
+# print(primeira_linha)
+
+# # Selecionando as 3 primeiras linhas
+# tres_linhas = df_filmes.iloc[0:3]
+# print('\nTrês primeiros filmes (DataFrame):')
+# print(tres_linhas)
+
+# # Selecionando linhas e colunas específicas
+# selecao_especifica = df_filmes.iloc[[0,3],[5,6]]
+# print("\n Printando uma seleção específica. \nLinha 0 e 3 com a coluna 5 e 6.")
+# print(selecao_especifica)
+
+# # Selecionando Dados com .loc
+# # Localiza os dados pelos rótulos
+# df_filmes_idx = df_filmes.set_index('Series_Title')
+# print('\n Definimos o índice agora como Series Titles.')
+# print(df_filmes_idx.head())
+
+# poderoso = df_filmes_idx.loc['The Godfather']
+# print("\nDados do filme: The Godfather: ")
+# print(poderoso)
+
+# # Filtrar os Dados baseados em condições (Boolean indexing0)
+# df_filmes_bem_avaliados = df_filmes[df_filmes['IMDB_Rating'] >= 8.5]
+# print("\n Filmes com nota >= 8.5 (Primeiras linhas).")
+# print(df_filmes_bem_avaliados['Series_Title'].head()) # Aparece só o título dos filmes
+
+# # Filmes que têm gênero 'Action'
+# filmes_acao = df_filmes[df_filmes['Genre'].str.contains('Action', na=False)]
+# print(" \n Filmes que contém o gênero 'Action'")
+# print(filmes_acao[['Series_Title', 'Genre']].head())
